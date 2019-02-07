6c1bf6c95bbea788e0df2e407c7ee376bfe355e0
didacta <igor.yermak@gmail.com>
2019-02-06 23:09:47 -0500

    light array

---
+++ array.js
@@ -0,0 +1,34 @@
+let coswave = [];
+
+function setup() {
+  createCanvas(500,660);
+  for (let i = 0; i < width; i++) {
+    let amount = map(i, 0, width, 0, PI);
+    coswave[i] = abs(cos(amount));
+  }
+  background(50,155,210,200);
+  noLoop();
+}
+
+function draw() {
+  let y1 = 0;
+  let y2 = height / 3;
+  for (let i = 0; i < width; i += 3) {
+    stroke(coswave[i] * 355);
+    line(i, y1, i, y2);
+  }
+
+  y1 = y2;
+  y2 = y1 + y1;
+  for (let i = 0; i < width; i += 3) {
+    stroke((coswave[i] * 255) / 8);
+    line(i, y1, i, y2);
+  }
+
+  y1 = y2;
+  y2 = height;
+  for (let i = 0; i < width; i += 3) {
+    stroke(255 - coswave[i] * 255);
+    line(i, y1, i, y2);
+  }
+}
