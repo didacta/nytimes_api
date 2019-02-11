66b77658c58f264418ac36b7f78d6e1c10583e28
didacta <igor.yermak@gmail.com>
2019-02-10 23:00:50 -0500

    adding lines and adjusting line formula

--- bool.js
+++ bool.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(700, 400);
+    createCanvas(800, 400);
     background(128,139,203);
     stroke(252,200,20);
   
@@ -17,7 +17,10 @@ function setup() {
   
       if (b === false) {
         // Horizontal line
-        line(middle, i - middle + d, width - d, i - middle + d);
+        line(middle, i - middle + d*10, width - d*143, i - middle + d*21);
+        line(middle, i - middle + d*10, width - d*13, i - middle + d*21);
+        line(middle, i - middle + d*50, width - d*13, i - middle + d*21);
+        line(middle, i + middle - d*50, width - d*13, i - middle + d*21);
       }
     }
   }
