d5706f86392ddabfc54d279f5808be807387ca77
didacta <igor.yermak@gmail.com>
2019-02-11 23:48:07 -0500

    color variables

---
+++ colorvar.js
@@ -0,0 +1,36 @@
+function setup() {
+    createCanvas(710, 400);
+    noStroke();
+    background(41, 210, 230);
+  
+    let inside = color(135, 202, 260);
+    let middle = color(24, 153, 250);
+    let outside = color(123, 151, 220);
+  
+    // These statements are equivalent to the statements above.
+    // Programmers may use the format they prefer.
+    //let inside = color('#CC6600');
+    //let middle = color('#CC9900');
+    //let outside = color('#993300');
+  
+    push();
+    translate(80, 80);
+    fill(outside);
+    rect(0, 0, 200, 200);
+    fill(middle);
+    rect(40, 60, 120, 120);
+    fill(inside);
+    rect(60, 90, 80, 80);
+    pop();
+  
+    push();
+    translate(360, 80);
+    fill(inside);
+    rect(0, 0, 200, 200);
+    fill(outside);
+    rect(40, 60, 120, 120);
+    fill(middle);
+    rect(60, 90, 80, 80);
+    pop();
+  }
+  
